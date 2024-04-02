a7b348ae1cf6f81636a5e5cb360abe0628f2e8df
LamLookmaxxing <tahuuphuclam3@gmail.com>
2024-04-02 14:44:34 +0700

    CreateArticle

--- src/components/CreateArticle.jsx
+++ src/components/CreateArticle.jsx
@@ -5,35 +5,34 @@ function CreateArticle() {
     <div className="editor-page">
       <div className="container page">
         <div className="row">
-          <div className="col-md-10 offset-md-1 col-xs-12">
-            <ul className="error-messages">
-              <li>That title is required</li>
-            </ul>
+          <div className="col-md-8 offset-md-2 col-xs-12">
 
             <form>
               <fieldset>
-                <fieldset className="form-group">
-                  <input type="text" className="form-control form-control-lg" placeholder="Article Title" />
+                <fieldset className="form-group mb-3 mt-3">
+                  <input type="text" className="form-control form-control-lg" placeholder="Article Title" style={{ fontSize: '22px' }} />
                 </fieldset>
-                <fieldset className="form-group">
+                <fieldset className="form-group mb-3">
                   <input type="text" className="form-control" placeholder="What's this article about?" />
                 </fieldset>
-                <fieldset className="form-group">
+                <fieldset className="form-group mb-3">
                   <textarea
                     className="form-control"
-                    rows="8"
+                    rows="7"
                     placeholder="Write your article (in markdown)"
                   ></textarea>
                 </fieldset>
-                <fieldset className="form-group">
+                <fieldset className="form-group mb-3">
                   <input type="text" className="form-control" placeholder="Enter tags" />
                   <div className="tag-list">
-                    <span className="tag-default tag-pill"> <i className="ion-close-round"></i> tag </span>
+                    <span className="tag-default tag-pill"> <i className="ion-close-round"></i></span>
                   </div>
                 </fieldset>
-                <button className="btn btn-lg pull-xs-right btn-primary" type="button">
+                <div className='text-end'>
+                <button className="btn btn-lg pull-xs-right btn-success mb-5" type="button">
                   Publish Article
                 </button>
+                </div>
               </fieldset>
             </form>
           </div>
