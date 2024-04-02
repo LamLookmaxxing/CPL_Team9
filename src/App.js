609a92e4eb1663e625efd0387824389818bea48b
Death0904 <nguyen.quan6c@gmail.com>
2024-04-02 13:27:01 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
+        <Headers />
         <Routes>
         <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
