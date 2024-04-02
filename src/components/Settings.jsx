ef5aa8e5f0532fe6c2d163b41809a95b147fb1f4
LamLookmaxxing <tahuuphuclam3@gmail.com>
2024-04-02 14:06:25 +0700

    finished settings and new articles

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -2,51 +2,51 @@ import React from 'react';
 
 function Settings() {
   return (
-    <div className="settings-page">
-      <div className="container page">
+    <div className="settings-page d-flex justify-content-center align-items-center">
+      <div className="container">
         <div className="row">
-          <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Your Settings</h1>
-
-            <ul className="error-messages">
-              <li>That name is required</li>
-            </ul>
+          <div className="col-md-6 offset-md-3">
+            <h1 className="text-center mt-3 mb-3">Your Settings</h1>
 
             <form>
-              <fieldset>
+              <fieldset className="centered-fieldset">
                 <fieldset className="form-group">
-                  <input className="form-control" type="text" placeholder="URL of profile picture" />
+                  <input className="form-control mb-3 " type="text" placeholder="URL of profile picture" />
                 </fieldset>
                 <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="text" placeholder="Your Name" />
+                  <input className="form-control mb-3" type="text" placeholder="Your Name" style={{ fontSize: '20px'}} />
                 </fieldset>
                 <fieldset className="form-group">
                   <textarea
-                    className="form-control form-control-lg"
+                    className="form-control mb-3"
                     rows="8"
                     placeholder="Short bio about you"
+                    style={{ fontSize: '20px' }}
                   ></textarea>
                 </fieldset>
                 <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="text" placeholder="Email" />
+                  <input className="form-control mb-3" type="email" placeholder="Email" style={{ fontSize: '20px' }} />
                 </fieldset>
                 <fieldset className="form-group">
                   <input
-                    className="form-control form-control-lg"
+                    className="form-control mb-3"
                     type="password"
                     placeholder="New Password"
+                    style={{ fontSize: '20px' }}
                   />
                 </fieldset>
-                <button className="btn btn-lg btn-primary pull-xs-right">Update Settings</button>
+                <div className="text-end">
+                  <button className="btn btn-lg btn-success">Update Settings</button>
+                </div>
               </fieldset>
             </form>
             <hr />
-            <button className="btn btn-outline-danger">Or click here to logout.</button>
+            <button className="btn btn-outline-danger btn-block mb-4">Or click here to logout.</button>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default Settings;
